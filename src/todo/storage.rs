5ea82d3d44fb9c369241f13d8ee7dd9b632f38dc
inksha <x1a010@163.com>
2025-06-27 17:06:07 +0800

    format docs and replace high level syntax

--- src/todo/storage.rs
+++ src/todo/storage.rs
@@ -4,11 +4,17 @@ use std::fs;
 pub fn read_todo_list(save_file: &str) -> Vec<TodoItem> {
     let mut result: Vec<TodoItem> = Vec::new();
 
-    if let Ok(content) = fs::read_to_string(save_file) {
-        if let Ok(mut list) = serde_json::from_str(content.as_str()) {
-            result.append(&mut list)
+    match fs::read_to_string(save_file) {
+        Ok(content) => match serde_json::from_str(content.as_str()) {
+            Ok(mut list) => result.append(&mut list),
+            _ => {
+                println!("parse file error");
+            }
+        },
+        _ => {
+            println!("read file error");
         }
-    };
+    }
 
     if result.len() == 0 {
         result.push(TodoItem::new("learn rust", "read rust book"));
