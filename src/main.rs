16ec9bb38708a48925f708613462ee9742b1facf
inksha <x1a010@163.com>
2025-06-27 10:34:01 +0800

    if expr

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ fn main() {
   let args: Vec<String> = env::args().collect();
   let len = args.len();
   let title = args[1].clone();
-  let mut content = String::from("default content");
 
-  if len > 2{
-    content = args[2].clone();
-  }
+  let content = if len > 2 {
+    args[2].clone()
+  } else {
+    String::from("default content")
+  };
 
   println!("todo title: {}, content: {}", title, content);
 }
